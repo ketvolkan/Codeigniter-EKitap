27be81e1668ffb9292ad10cb82672fd8026f828c
ketvolkan <ketvolkan1@gmail.com>
2022-04-24 01:01:32 +0300

    klasörler ayrılıdı temel controllerlar oluşturuldu

---
+++ application/views/ekitap.php
@@ -0,0 +1,12 @@
+<html lang="en">
+
+<head>
+    <?php $this->load->view("includes/header"); ?>
+</head>
+
+<body>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero laborum sunt dolorum neque quidem quam, omnis tempore eligendi ducimus similique voluptates veritatis nulla, ex ea est accusamus eum numquam suscipit.</p>
+    <?php $this->load->view("includes/footer"); ?>
+</body>
+
+</html>
