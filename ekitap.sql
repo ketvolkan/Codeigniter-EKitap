2ebe089acfa8b0e17f34b6a09b97a2fb87e8a8ed
ketvolkan <ketvolkan1@gmail.com>
2022-04-26 02:38:33 +0300

    veritabanı güncel hali eklendi

--- ekitap.sql
+++ ekitap.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 23, 2022 at 11:55 ÖS
+-- Generation Time: Apr 26, 2022 at 01:37 ÖÖ
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -23,6 +23,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `settings`
+--
+
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `site_name` varchar(50) NOT NULL,
+  `navbar_title` varchar(50) NOT NULL,
+  `hakkımızda_description` text NOT NULL,
+  `footer` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `site_name`, `navbar_title`, `hakkımızda_description`, `footer`) VALUES
+(1, 'E-Kitap', 'E-Kitap', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.', 'Copyright ©2022');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -33,7 +55,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(200) NOT NULL,
   `password` varchar(16) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstName`, `lastName`, `email`, `password`) VALUES
+(1, 'Volkan', 'Ket', 'ketvolkan1@gmail.com', '1616');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
